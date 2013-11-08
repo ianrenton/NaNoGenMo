70bc32514c31b6897d9b1ac9111e213eef546bcc
Ian Renton <ian@ianrenton.com>
2013-11-08 18:33:32 +0000

    Story now split into chapters. Closes #6.

--- nanogenmo.rb
+++ nanogenmo.rb
@@ -2,7 +2,7 @@
 # encoding: UTF-8
 
 # Generates random fiction by harvesting stories from Fanfiction.net and semi-randomly
-# combining the sentences it finds.
+# combining the @sentences it finds.
 
 require 'rubygems'
 require 'nokogiri'
@@ -22,8 +22,8 @@ require 'redcarpet'
 INDEX_URL = 'http://www.fanfiction.net/tv/Doctor-Who/'
 
 # A list of words which, if present in a sentence, will disqualify it from use in
-# the generator. Used to catch sentences which aren't part of the actual text.
-BANNED_WORDS = ['Chapter', 'review', 'A/N', 'Note']
+# the generator. Used to catch @sentences which aren't part of the actual text.
+BANNED_WORDS = ['Chapter', 'review', 'A/N', 'Note', '*', '1.', '2.', '3.', '4.', '5.', '6.', '7.', '8.', '9.', '0.', 'R&R']
 
 #########################################
 #         WEB SCRAPING CONFIG           #
@@ -32,7 +32,7 @@ BANNED_WORDS = ['Chapter', 'review', 'A/N', 'Note']
 # Fetch live data from the web. "true" is the normal use case. If you have previously run
 # the script and want to run it again to get a new story with the same data set (i.e.
 # without spending ages scraping data from the web again) you can set this to "false".
-FETCH_LIVE_DATA = false
+FETCH_LIVE_DATA = true
 
 # Stop after finding this many pages to avoid huge data sets
 MAX_PAGES = 100
@@ -47,7 +47,10 @@ PAGE_DELAY = 5
 #########################################
 
 # Number of words to aim for.
-WORD_GOAL = 1000
+WORD_GOAL = 50000
+
+# Number of chapters to write. Each will be roughly WORD_GOAL/NUM_CHAPTERS words long.
+NUM_CHAPTERS = 20
 
 # For every 1 proper paragraph, there will be this many solitary (one-sentence)
 # paragraphs.
@@ -56,10 +59,10 @@ SOLITARY_RATE = 0.1
 # For every 1 proper paragraph, there will be this many dialogue sequences.
 DIALOGUE_RATE = 0.2
 
-# Maximum number of sentences per paragraph
+# Maximum number of @sentences per paragraph
 MAX_SENT_PER_PARA = 6
 
-# Maximum number of sentences / lines in a dialogue sequence.
+# Maximum number of @sentences / lines in a dialogue sequence.
 MAX_SENT_PER_DIALOGUE = 6
 
 #########################################
@@ -73,13 +76,30 @@ USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:24.0) Gecko/20100101 Fi
 DATA_CACHE_FILE_NAME = 'cache.yaml'
 STORY_MARKDOWN_FILE_NAME = 'story.md'
 STORY_HTML_FILE_NAME = 'story.html'
-# Element IDs, classes and regexes to find and extract stories, pages, and sentences.
+# Element IDs, classes and regexes to find and extract stories, pages, and @sentences.
 STORY_LINK_CLASS = 'stitle'
 CHAPTER_SELECT_ID = 'chap_select'
 STORY_TEXT_ID = 'storytext'
 SENTENCE_REGEX = /[^.!?\s][^.!?]*(?:[.!?](?!['"]?\s|$)[^.!?]*)*[.!?]?['"]?(?=\s|$)/
 QUOTED_TEXT_REGEX = /"([^"]*)"/
 STRIP_FROM_TITLE_REGEX = /[\,\.\"]/
+
+#########################################
+#          METHODS START HERE           #
+#########################################
+
+# Makes a title for the story or a chapter
+def makeTitle()
+  title = ''
+  while title == ''
+		tmpTitle = @sentences[:dialogue][rand(@sentences[:dialogue].size - 1)]
+		quotedSections = tmpTitle.scan(QUOTED_TEXT_REGEX)
+		if !quotedSections.nil? && !quotedSections[0].nil?
+			title = quotedSections[0][0].gsub(STRIP_FROM_TITLE_REGEX, '')
+		end
+	end
+	return title
+end
 
 #########################################
 #           CODE STARTS HERE            #
@@ -140,7 +160,7 @@ if FETCH_LIVE_DATA
 
 	# Create a data structure that will hold each sentence in an array, sorted by which
 	# type of sentence it is.
-	sentences = {
+	@sentences = {
 		:startChapters => [],
 		:endChapters => [],
 		:startParagraphs => [],
@@ -150,8 +170,8 @@ if FETCH_LIVE_DATA
 		:dialogue => []
 	}
 	
-	# For each page URL, load the page and extract sentences.
-	print 'Extracting sentences'
+	# For each page URL, load the page and extract @sentences.
+	print 'Extracting @sentences'
   pageURLs.each do |pageURL|
     print '.'
     begin
@@ -159,7 +179,7 @@ if FETCH_LIVE_DATA
   		sleep(PAGE_DELAY)
 			paragraphs = pageHTML.css("div\##{STORY_TEXT_ID} p")
 			paragraphs.each_with_index do |para, pi|
-				# Take the contents of each <p> element, remove linebreaks and scan for sentences
+				# Take the contents of each <p> element, remove linebreaks and scan for @sentences
 				tmpSentences = para.text.tr("\n"," ").tr("\r"," ").scan(SENTENCE_REGEX)
 				tmpSentences.each_with_index do |tmpSentence, i|
 				  # Check for 'banned' words, only proceed if they are not present
@@ -167,19 +187,19 @@ if FETCH_LIVE_DATA
 				    # Based on the sentence's position and content, decide what type it is and
 				    # thus into which bucket it goes.
 						if (pi == 0) && (i == 0)
-							sentences[:startChapters] << tmpSentence
+							@sentences[:startChapters] << tmpSentence
 						elsif (pi == paragraphs.size - 1) && (i == tmpSentences.size - 1)
-							sentences[:endChapters] << tmpSentence
+							@sentences[:endChapters] << tmpSentence
 						elsif tmpSentence.include? '"'
-							sentences[:dialogue] << tmpSentence
+							@sentences[:dialogue] << tmpSentence
 						elsif tmpSentences.size == 1
-							 sentences[:solitary] << tmpSentence
+							 @sentences[:solitary] << tmpSentence
 						elsif i == 0
-							 sentences[:startParagraphs] << tmpSentence
+							 @sentences[:startParagraphs] << tmpSentence
 						elsif i == tmpSentences.size - 1
-							 sentences[:endParagraphs] << tmpSentence
+							 @sentences[:endParagraphs] << tmpSentence
 						else
-							 sentences[:midParagraphs] << tmpSentence
+							 @sentences[:midParagraphs] << tmpSentence
 						end
 					end
 				end
@@ -188,11 +208,11 @@ if FETCH_LIVE_DATA
 	    print "\nFailed to load and parse a page. Carrying on..."
 	  end
   end
-  print " #{sentences[:startChapters].size + sentences[:endChapters].size + sentences[:startParagraphs].size + sentences[:midParagraphs].size + sentences[:endParagraphs].size + sentences[:solitary].size + sentences[:dialogue].size} found.\n"
+  print " #{@sentences[:startChapters].size + @sentences[:endChapters].size + @sentences[:startParagraphs].size + @sentences[:midParagraphs].size + @sentences[:endParagraphs].size + @sentences[:solitary].size + @sentences[:dialogue].size} found.\n"
 	
 	# Serialise the data to disk for later use
 	print "Saving data to #{DATA_CACHE_FILE_NAME}..."
-	serialisedSentences = YAML::dump(sentences)
+	serialisedSentences = YAML::dump(@sentences)
 	File.open(DATA_CACHE_FILE_NAME, 'w') { |file| file.write(serialisedSentences) }
 	print " Done.\n"
 
@@ -202,8 +222,8 @@ else
 	if File.file?(DATA_CACHE_FILE_NAME)
 		print "Loading data from #{DATA_CACHE_FILE_NAME}..."
 		serialisedSentences = File.read(DATA_CACHE_FILE_NAME)
-		sentences = YAML::load(serialisedSentences)
-		print " #{sentences[:startChapters].size + sentences[:endChapters].size + sentences[:startParagraphs].size + sentences[:midParagraphs].size + sentences[:endParagraphs].size + sentences[:solitary].size + sentences[:dialogue].size} sentences loaded.\n"
+		@sentences = YAML::load(serialisedSentences)
+		print " #{@sentences[:startChapters].size + @sentences[:endChapters].size + @sentences[:startParagraphs].size + @sentences[:midParagraphs].size + @sentences[:endParagraphs].size + @sentences[:solitary].size + @sentences[:dialogue].size} @sentences loaded.\n"
 	else
 	  # No file, so error out
 		print "FETCH_LIVE_DATA was set to 'false' but a data file named #{DATA_CACHE_FILE_NAME} could not be found. This means there is no source of data for the script to use. Check your configuration.\n"
@@ -213,61 +233,68 @@ else
 end
 
 
-# Start generating. If we get here, we know that sentences has contents that we can use.
+# Start generating. If we get here, we know that @sentences has contents that we can use.
 story = ''
 
 # Write a title. Steal a line of dialog so we have something 'punchy' (or just weird)
 # Underline it in Markdown style.
 print 'Writing a story called... '
-title = ''
-while title == ''
-	tmpTitle = sentences[:dialogue][rand(sentences[:dialogue].size - 1)]
-	quotedSections = tmpTitle.scan(QUOTED_TEXT_REGEX)
-	if !quotedSections.nil? && !quotedSections[0].nil?
-	  title = quotedSections[0][0].gsub(STRIP_FROM_TITLE_REGEX, '')
-	end
-end
+title = makeTitle()
 story << title << "\n" << "="*title.length << "\n\n"
 print "\"#{title}\".\n"
 
 print 'Generating text.'
-# Start with an opening sentence
-story << sentences[:startChapters][rand(sentences[:startChapters].size - 1)] << "\n\n"
 
-# Keep going until word count goal is reached
-while story.split.size < WORD_GOAL
-  print '.'
-  # Decide what type of section we are going into - a proper paragraph (at least 2 
-  # sentences), a solitary sentence, or a dialogue section.
-  roll = rand * (1 + SOLITARY_RATE + DIALOGUE_RATE)
-  if roll < SOLITARY_RATE
-    # Solitary. Pick a solitary paragraph and concatenate it to the story.
-    story << sentences[:solitary][rand(sentences[:solitary].size - 1)] << "\n\n"
-  elsif roll < (SOLITARY_RATE + DIALOGUE_RATE)
-    # Dialogue. First work out how long the dialogue should be.
-    dialogueLength = rand(MAX_SENT_PER_DIALOGUE)
-    # Now add that many dialogue paragraphs.
-    for i in 0..dialogueLength
-      story << sentences[:dialogue][rand(sentences[:dialogue].size - 1)] << "\n\n"
-    end
-  else
-    # Normal Paragraph. First work out how long the paragraph should be. Must be at
-    # least 2
-    paragraphLength = rand(MAX_SENT_PER_PARA - 1) + 1
-    # Now add a beginning sentence, the right number of middle sentences, and an end
-    # sentence.
-    story << sentences[:startParagraphs][rand(sentences[:startParagraphs].size - 1)] << ' '
-    for i in 0..paragraphLength-2
-      story << sentences[:midParagraphs][rand(sentences[:midParagraphs].size - 1)] << ' '
-    end
-    story << sentences[:endParagraphs][rand(sentences[:endParagraphs].size - 1)] << "\n\n"
-  end
+# Word out how long each chapter should be
+chapterLength = WORD_GOAL / NUM_CHAPTERS
+
+# Generate a number of chapters
+for chapterNumber in 1..NUM_CHAPTERS
+
+  # Insert a chapter heading
+  chapterTitle = makeTitle()
+  chapterHeading = "Chapter #{chapterNumber}. #{chapterTitle}"
+  story << chapterHeading << "\n" << "="*chapterHeading.length << "\n\n"
+
+	# Start the chapter with an opening sentence
+	story << @sentences[:startChapters][rand(@sentences[:startChapters].size - 1)] << "\n\n"
+
+	# Keep going until word count goal for this chapter is reached
+	while story.split.size < chapterLength * chapterNumber
+		print '.'
+		# Decide what type of section we are going into - a proper paragraph (at least 2 
+		# @sentences), a solitary sentence, or a dialogue section.
+		roll = rand * (1 + SOLITARY_RATE + DIALOGUE_RATE)
+		if roll < SOLITARY_RATE
+		  # Solitary. Pick a solitary paragraph and concatenate it to the story.
+		  story << @sentences[:solitary][rand(@sentences[:solitary].size - 1)] << "\n\n"
+		elsif roll < (SOLITARY_RATE + DIALOGUE_RATE)
+		  # Dialogue. First work out how long the dialogue should be.
+		  dialogueLength = rand(MAX_SENT_PER_DIALOGUE)
+		  # Now add that many dialogue paragraphs.
+		  for i in 0..dialogueLength
+		    story << @sentences[:dialogue][rand(@sentences[:dialogue].size - 1)] << "\n\n"
+		  end
+		else
+		  # Normal Paragraph. First work out how long the paragraph should be. Must be at
+		  # least 2
+		  paragraphLength = rand(MAX_SENT_PER_PARA - 1) + 1
+		  # Now add a beginning sentence, the right number of middle @sentences, and an end
+		  # sentence.
+		  story << @sentences[:startParagraphs][rand(@sentences[:startParagraphs].size - 1)] << ' '
+		  for i in 0..paragraphLength-2
+		    story << @sentences[:midParagraphs][rand(@sentences[:midParagraphs].size - 1)] << ' '
+		  end
+		  story << @sentences[:endParagraphs][rand(@sentences[:endParagraphs].size - 1)] << "\n\n"
+		end
+	end
+
+	# Finish the chapter with a closing sentence
+	story << @sentences[:endChapters][rand(@sentences[:endChapters].size - 1)] << "\n\n"
+
 end
 
-# Finish with a closing sentence
-story << sentences[:endChapters][rand(sentences[:endChapters].size - 1)] << "\n\n"
-
-print " wrote #{story.split.size} words!\n"
+print " wrote #{story.split.size} words in #{NUM_CHAPTERS} chapters!\n"
 
 # Save the file as markdown
 print 'Saving file...'
@@ -280,3 +307,5 @@ markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
 html = markdown.render(story)
 File.open(STORY_HTML_FILE_NAME, 'w') {|f| f.write(html) }
 print " done.\n"
+
+
